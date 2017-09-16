f720ec688ee74aa16c58c15bda6c54822b712d93
rSterling319 <richard.sterling.319@gmail.com>
2017-09-15 20:44:33 -0600

    Edited config.py

--- config.py
+++ config.py
@@ -12,3 +12,8 @@ NUMBER_OF_COL = 3
 DIMENSION_OF_EACH_SQUARE = 124 #124pixels
 X_AXIS_LABELS = ('A', 'B', 'C')
 Y_AXIS_LABELS = (1, 2, 3)
+BOARD_START = {
+    'A1':'', 'A2':'', 'A3':'',
+    'B1':'', 'B2':'', 'B3':'',
+    'C1':'', 'C2':'', 'C3':'',
+}
